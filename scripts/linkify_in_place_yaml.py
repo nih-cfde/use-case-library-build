e8661dc0fc9ab78fd8dae8af3c7e8fdbc30368b3
C Reid <charlesreid1@Gmail.com>
2018-10-13 09:51:13 -0700

    rename linkify script

--- scripts/linkify_in_place_yaml.py
+++ scripts/linkify_in_place_yaml.py
@@ -1,26 +1,41 @@
 #!/usr/bin/env python
 import os, re, sys
 import subprocess
+from parse_input_files import parse_library_md
 
 """
-Linkify all links in a document using Pandoc
+Linkify Script for Markdown + YAML Headers
+
+This script linkifies the body of Markdown documents
+with YAML headers by removing the header, using Pandoc
+to linkify the body, and reattaching the header.
+
+The linkification step uses Pandoc to convert
+Github-flavored markdown to Github-flavored Markdown.
+
+There is an optional step in the middle to apply
+a panflute filter.
+
+There is also a step at the end to apply a regular
+expression filter.
 """
 
 def usage():
-    print("linkify_in_place:")
-    print("This script will search a pile of Markdown files for")
-    print("hyperlinks and turn them into markdownified hyperlinks")
-    print("in-place. WARNING: This is a destructive task.")
+    print("linkify_in_place_yaml.py:")
+    print("This script will search a pile of Markdown files with")
+    print("YAML headers for hyperlinks and turn them into markdownified")
+    print("hyperlinks in-place.")
+    print("WARNING: This is a destructive task.")
     print("")
     print("Usage:")
-    print("    ./linkify_in_place [FLAGS] <path-to-markdown-files>")
+    print("    ./linkify_in_place_yaml.py [FLAGS] <path-to-markdown-files>")
     print("")
     print("        -n | --dry-run       Print the names of files that would be")
     print("                             changed if the linkify_in_place script")
     print("                             were run.")
     print("")
     print("Example:")
-    print("    ./linkify_in_place ../library")
+    print("    ./linkify_in_place_yaml.py ../library")
     print("")
     exit(1)
 
@@ -55,37 +70,39 @@ def main():
 
             if( bool1 and bool2 and bool3):
                 markdown_files.append( os.path.join( fdir, f ) )
-    
+
+    # Here is the strategy:
+    # For each markdown document:
+    #    Tear off the yaml header
+    #    Make a secondary file (body only)
+    #    Apply pandoc gfm to json
+    #    (Apply panflute filter to linkify)
+    #    Apply json to gfm
+    #    Paste yaml header back on
+    #    Output to file 
+
 
     # Linkify each markdown document found
     for md in markdown_files:
     
         print("-"*40,file=sys.stderr)
         print("Linkifying (in-place) document: %s"%(md),file=sys.stderr)
-    
-        if dry_run is False:
 
+        if (dry_run is False) or (True):
 
+            yaml_header, body = parse_library_md(md)
 
-            # NOTES:
+            # make a new backup file
+            backup_md = re.sub('.md$','.md.secondary',md)
 
-            # Make a new backup file,
-            # tear off the yaml header,
-            # apply the pandoc gfm to gfm 
-            # conversion step, then paste
-            # the yaml header back on.
-            # 
-            # Once that is all done,
-            # move the backup file to 
-            # the real file.
+            with open(backup_md,'w') as f:
+                f.write("\n".join(body))
 
-
-
-            # print md to screen
-            cat_cmd = ['cat', md]
+            # cat md (we will pass this to pandoc)
+            cat_cmd = ['cat', backup_md]
             cat_proc = subprocess.Popen(cat_cmd,
                     stdout=subprocess.PIPE)
-    
+
             # pandoc: md to json
             pandoc_from_cmd = ['pandoc','-f','gfm','-t','json','-s']
             pandoc_from_proc = subprocess.Popen(pandoc_from_cmd, 
@@ -127,7 +144,7 @@ def main():
             # we now have the text of the pandoc output
             pandoc_output = pandoc_to_proc.stdout.read().decode('utf-8')
 
-            # apply any filtering at this point
+            # apply any regex filtering at this point
             # 
             # github checkboxes (not handled well by pandoc...)
             # \[x\] or \[X\] into [x]
@@ -141,9 +158,18 @@ def main():
             # target file is same as source/input file
             target = md
 
+            delim = '---\n'
+
             # write to target file
             with open(target,'w') as f:
-                f.write(final_document)
+                f.write(delim)
+                for key in yaml_header.keys():
+                    f.write("%s: %s\n"%(key,yaml_header[key]))
+                f.write(delim)
+                f.write("\n".join(body))
+
+            # remove backup file
+            os.remove(backup_md)
 
             print("Finished linkifying document: %s"%(target),file=sys.stderr)
 
